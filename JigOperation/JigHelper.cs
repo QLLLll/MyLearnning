39d25c57515bd859183aaafcd1f4ba1d032a1a4a
刘强 <GGBman@hotmail.com>
2019-09-17 17:48:42 +0800

    jigoperation

---
+++ JigOperation/JigHelper.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.GraphicsInterface;
+using Autodesk.AutoCAD.Geometry;
+
+namespace JigOperation
+{
+    public class JigHelper : DrawJig
+    {
+        private Func<JigPrompts, SamplerStatus> inputFunc;
+        private Action<JigHelper> updateFunc;
+        private Entity[] entities;
+
+        public Point3d Point { get; set; }
+        public double Distance { get; set; }
+        public double Angle { get; set; }
+
+        public void PrapareForNextInput(JigPromptPointOptions opt, string message)
+        {
+            inputFunc = (prompts) =>
+            {
+                opt.Message = message;
+
+                var res = prompts.AcquirePoint(opt);
+                if (res.Value != Point)
+                {
+                    Point = res.Value;
+                }
+                else
+                {
+                    return SamplerStatus.NoChange;
+                }
+
+                if (res.Status == PromptStatus.OK)
+                {
+                    return SamplerStatus.OK;
+                }
+                else
+                {
+                    return SamplerStatus.Cancel;
+                }
+            };
+        }
+
+        public void PrapareForNextInput(JigPromptDistanceOptions opt, string message)
+        {
+            inputFunc = (prompts) =>
+            {
+                opt.Message = message;
+
+                var res = prompts.AcquireDistance(opt);
+                if (res.Value != Distance)
+                {
+                    Distance = res.Value;
+                }
+                else
+                {
+                    return SamplerStatus.NoChange;
+                }
+
+                if (res.Status == PromptStatus.OK)
+                {
+                    return SamplerStatus.OK;
+                }
+                else
+                {
+                    return SamplerStatus.Cancel;
+                }
+            };
+        }
+
+        public void PrapareForNextInput(JigPromptAngleOptions opt, string message)
+        {
+            inputFunc = (prompts) =>
+            {
+                opt.Message = message;
+
+                var res = prompts.AcquireAngle(opt);
+                if (res.Value != Angle)
+                {
+                    Angle = res.Value;
+                }
+                else
+                {
+                    return SamplerStatus.NoChange;
+                }
+
+                if (res.Status == PromptStatus.OK)
+                {
+                    return SamplerStatus.OK;
+                }
+                else
+                {
+                    return SamplerStatus.Cancel;
+                }
+            };
+        }
+
+        public void SetEntities(IEnumerable<Entity> ents)
+        {
+            entities = ents?.ToArray();
+        }
+
+        public void SetUpdate(Action<JigHelper> upd)
+        {
+            updateFunc = upd;
+        }
+
+        public PromptStatus Drag()
+        {
+            return Application.DocumentManager.MdiActiveDocument.Editor
+                .Drag(this).Status;
+        }
+
+        protected override SamplerStatus Sampler(JigPrompts prompts)
+        {
+            return inputFunc.Invoke(prompts);
+        }
+
+        protected override bool WorldDraw(WorldDraw draw)
+        {
+            if (entities != null)
+            {
+                updateFunc(this);
+                foreach (var ent in entities)
+                {
+                    ent.WorldDraw(draw);
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ JigOperation/JigOperation.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.GraphicsInterface;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(JigOperation.JigOperation))]
+[assembly: ExtensionApplication(typeof(JigOperation.JigOperation))]
+
+namespace JigOperation
+{
+    public class JigOperation : IExtensionApplication
+    {
+        [CommandMethod("cmd1")]
+        public void Cmd1()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+
+            Editor ed = doc.Editor;
+
+            var bsPointRes = ed.GetPoint(new PromptPointOptions("\n请输入圆心"));
+
+            if (bsPointRes.Status == PromptStatus.OK)
+            {
+
+                var jigdistanceOpts = new JigPromptDistanceOptions();
+
+                jigdistanceOpts.BasePoint = bsPointRes.Value;
+
+                jigdistanceOpts.UseBasePoint = true;
+
+                var circle1 = new Circle() { Center = jigdistanceOpts.BasePoint };
+                var circle2 = new Circle() { Center = jigdistanceOpts.BasePoint };
+                var hatch = new Hatch();
+
+                hatch.SetHatchPattern(HatchPatternType.PreDefined, "BOX");
+                hatch.PatternScale = 0.01;
+
+                var donut = new JigHelper();
+
+                donut.PrapareForNextInput(jigdistanceOpts, "\n请输入第一圈");
+                donut.SetEntities(new Entity[] { circle1 });
+                donut.SetUpdate(jig => { circle1.Radius = Math.Max(0.1, jig.Distance); });
+                if (donut.Drag() != PromptStatus.OK)
+                {
+                    return;
+                }
+
+
+                donut.PrapareForNextInput(jigdistanceOpts, "\n请输入第二圈");
+                donut.SetEntities(new Entity[] { circle1, circle2, hatch });
+                donut.SetUpdate(jig => { circle2.Radius = Math.Max(0.1, jig.Distance); });
+
+                if (donut.Drag() != PromptStatus.OK)
+                {
+                    return;
+                }
+
+                hatch.AppendLoop(HatchLoopTypes.Default, new Curve2dCollection()
+                { new CircularArc2d(new Point2d(circle1.Center.X,circle1.Center.Y),circle1.Radius)},
+                new IntegerCollection() { 0 });
+
+                hatch.AppendLoop(HatchLoopTypes.Default, new Curve2dCollection()
+                { new CircularArc2d(new Point2d(circle2.Center.X,circle2.Center.Y),circle2.Radius)},
+                new IntegerCollection() { 0 });
+
+                if (donut.Drag() != PromptStatus.OK)
+                {
+                    return;
+                }
+
+                circle1.ToSpace();
+                circle2.ToSpace();
+                hatch.ToSpace();
+            }
+
+
+        }
+
+        [CommandMethod("cmd2")]
+        public void Cmd2()
+        {
+
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+
+            Editor ed = doc.Editor;
+
+            Autodesk.AutoCAD.DatabaseServices.Polyline pyLine = new Autodesk.AutoCAD.DatabaseServices.Polyline();
+
+            Point3d pt1 = new Point3d();
+
+            var bsPointRes = ed.GetPoint(new PromptPointOptions("\n请输入圆心"));
+
+            if (bsPointRes.Status == PromptStatus.OK)
+            {
+
+                pt1 = bsPointRes.Value;
+
+                pyLine.AddVertexAt(pyLine.NumberOfVertices, new Point2d(bsPointRes.Value.X, bsPointRes.Value.Y), 0, 0, 0);
+                var jigPtOpts = new JigPromptPointOptions();
+
+                var dimAlign = new AlignedDimension();
+
+                var donut = new JigHelper();
+
+                donut.SetEntities(new Entity[] { pyLine,dimAlign });
+
+                for (int i = 1; i < int.MaxValue; i++)
+                {
+
+                    donut.PrapareForNextInput(jigPtOpts, "\n请输入下一个点");
+
+                   
+                        donut.SetUpdate(jig =>
+                        {
+
+                            if (pyLine.NumberOfVertices == i)
+                            {
+                                pyLine.AddVertexAt(pyLine.NumberOfVertices, new Point2d(jig.Point.X, jig.Point.Y), 0, 0, 0);
+                            }
+                            else
+                            {
+                                pyLine.SetPointAt(i, new Point2d(jig.Point.X, jig.Point.Y));
+                            }
+                           if (i >= 1)
+                            {
+
+
+                                Point3d pt2 = jig.Point;
+
+                                var vec3d = pt1 - pt2;
+                                var vec3d2 = vec3d.RotateBy(Math.PI / 2, Vector3d.ZAxis);
+
+                             
+
+                                Point3d cPoint3d = new Point3d((pt1.X + pt2.X) / 2, (pt1.Y + pt2.Y) / 2, 0);
+
+                                dimAlign.XLine1Point = pt1;
+                                dimAlign.XLine2Point= pt2;
+
+
+
+                                dimAlign.DimLinePoint = cPoint3d + vec3d2 * 2.5;
+
+                                dimAlign.DimensionText = null;
+
+                                dimAlign.TextStyleId = DBHelper.GetSymbol(doc.Database.DimStyleTableId, "Standard");
+
+                                pt1 = pt2;
+                            }
+                        });
+                   
+
+                var status = donut.Drag();
+                if (status == PromptStatus.Cancel)
+                {
+                    break;
+                }
+                else if (status != PromptStatus.OK)
+                {
+                    return;
+                }
+                   
+                }
+                dimAlign.ToSpace();
+                pyLine.ToSpace();
+               
+
+
+            }
+
+
+    }
+
+    public void Initialize()
+    {
+
+    }
+
+    public void Terminate()
+    {
+
+    }
+}
+}
