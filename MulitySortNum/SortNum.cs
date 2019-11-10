d697508c7d0aede0ed70dc2f8743da0b02edfa94
刘强 <GGBman@hotmail.com>
2019-11-10 21:36:18 +0800

    编号

--- MulitySortNum/SortNum.cs
+++ MulitySortNum/SortNum.cs
@@ -20,23 +20,24 @@ namespace MulitySortNum
         private string str = "LL";
         public int index = 1;
 
-        [CommandMethod("NumSort")]
-        public void NumSort() {
+        [CommandMethod("NumSort0")]
+        public void NumSort()
+        {
 
             ObjectId lyId = ObjectId.Null;
 
             using (Transaction trans = db.TransactionManager.StartTransaction())
             {
 
-                var lyBlk = trans.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
-                var tblBlk = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                var lyTbl = trans.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
+                //var blkTbl = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
 
                 LayerTableRecord lyTblRec = null;
 
-                if (lyBlk.Has("编号"))
+                if (lyTbl.Has("编号"))
                 {
 
-                    lyTblRec = trans.GetObject(lyBlk["编号"], OpenMode.ForRead) as LayerTableRecord;
+                    lyTblRec = trans.GetObject(lyTbl["编号"], OpenMode.ForRead) as LayerTableRecord;
 
                 }
 
@@ -66,9 +67,9 @@ namespace MulitySortNum
 
 
                 SelectionFilter sf = new SelectionFilter(new[] { typedValue });
-                 
 
-                PromptSelectionResult sRes = ed.GetSelection(sOpts,sf);
+
+                PromptSelectionResult sRes = ed.GetSelection(sOpts, sf);
 
                 ObjectIdCollection ids = new ObjectIdCollection();
 
@@ -91,6 +92,9 @@ namespace MulitySortNum
                         return;
                     }
 
+
+
+
                     Point3d pt = br.Position;
 
                     DBText dbText = new DBText();
@@ -100,26 +104,87 @@ namespace MulitySortNum
                     dbText.Position = new Point3d(pt.X, pt.Y - 100, pt.Z);
 
                     DBHelper.ToSpace(dbText);
-
-
-
                 }
-
-
-
-
-
-
-
                 trans.Commit();
-
             }
-
-
 
         }
 
+        [CommandMethod("NumSort1")]
+        public void NumSort1()
+        {
+            index = 1;
+
+            var propEnt = new PromptEntityOptions("请选择要编号的一个块\n");
+
+            var propRes = ed.GetEntity(propEnt);
+
+            if (propRes.Status != PromptStatus.OK)
+            {
+                return;
+            }
+
+            var oId = propRes.ObjectId;
+
+            ObjectIdCollection objIds = null;
+            List<DBText> listDBText = new List<DBText>();
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+               var blkRef = trans.GetObject(oId, OpenMode.ForRead) as BlockReference;
 
 
+
+                if (blkRef == null)
+                {
+                    Application.ShowAlertDialog("请选择块定义");
+                    return;
+                }
+
+                var recId = blkRef.BlockTableRecord;
+
+                var blkTblRec = trans.GetObject(recId, OpenMode.ForRead) as BlockTableRecord;
+
+                objIds = blkTblRec.GetBlockReferenceIds(true, false);
+
+                
+
+                var txtStlTbl = trans.GetObject(db.TextStyleTableId, OpenMode.ForRead) as TextStyleTable;
+
+                var txtstyleId = txtStlTbl["Standard"];
+
+                List<BlockReference> listBr = new List<BlockReference>();
+
+                foreach (ObjectId objectId in objIds)
+                {
+                   var blkTempRef = trans.GetObject(objectId, OpenMode.ForRead) as BlockReference;
+
+                    listBr.Add(blkTempRef);
+
+                    
+                }
+
+                listBr.OrderByDescending(b => b.Position.Y).ToList().ForEach(blkTempRef =>
+                {
+
+                    DBText dbText = new DBText();
+                    dbText.TextString = str + "_" + index++;
+                    dbText.TextStyleId = txtstyleId;
+
+                    var pointMin = blkTempRef.Bounds.Value.MinPoint;
+                    var pointMax = blkTempRef.Bounds.Value.MaxPoint;
+                    dbText.HorizontalMode = TextHorizontalMode.TextMid;
+                    dbText.AlignmentPoint = pointMin + Vector3d.YAxis * 2 + Vector3d.XAxis * Math.Abs(pointMax.X - pointMin.X) / 2;
+
+                    listDBText.Add(dbText);
+
+                });
+
+                trans.Commit();
+            }
+
+            listDBText.ToSpace();
+
+        }
     }
 }
